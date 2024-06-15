0f4db1688285cb746d2eec9d4978c64700586d9d
Gonzalibrandi <librandigonzalo@gmail.com>
2024-06-14 23:23:56 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const PassportLocal = require('passport-local').Strategy;
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+// Configuración del motor de vistas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Middleware básicos
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuración de sesión para Passport
+app.use(session({
+  secret: 'secreto',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Inicialización de Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Estrategia de autenticación local con Passport
+passport.use(new PassportLocal(function(username, password, done) {
+  if (username === "poli" && password === "12345") {
+    return done(null, { id: 1, name: "poli" });
+  }
+  done(null, false);
+}));
+
+// Serialización de usuario
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+// Deserialización de usuario
+passport.deserializeUser(function(id, done) {
+  done(null, { id: 1, name: "poli" });
+});
+
+// Rutas para manejar la autenticación
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/', // Redirigir a el buscador si la autenticación es exitosa
+  failureRedirect: '/login'    // Redirigir a /login si la autenticación falla
+}));
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// Middleware para proteger rutas
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+// Ruta para la página principal protegida
+app.get('/', ensureAuthenticated, (req, res) => {
+  res.render('index', { user: req.user });
+});
+
+// Rutas adicionales (usersRouter podría contener rutas de usuarios, por ejemplo)
+app.use('/users', usersRouter);
+
+// Manejo de errores 404
+app.use(function(req, res, next) {
+  res.status(404).send('Página no encontrada');
+});
+
+// Manejo de errores
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Error interno del servidor');
+});
+
+module.exports = app;
---
+++ configuracion-indice.js
@@ -0,0 +1,93 @@
+const { MeiliSearch } = require('meilisearch'); // Importación correcta
+
+// Configurar conexión con Meilisearch
+const client = new MeiliSearch({
+  host: 'http://meilisearch:7700',
+  apiKey: 'MASTER_KEY'
+});
+
+
+/* con esto tomamos el archivo proyectos y creamos un indice con ese mismo nombre y con el archivo */
+/* 
+const proyectos = require('./proyectos.json');
+client.index('proyectos').addDocuments(proyectos).then((res) => console.log(res)).catch((err) => console.error(err));
+*/
+
+/* actualizacion de atributos displayed */
+/*
+client.index('proyectos').updateSettings({
+  "rankingRules": [
+      "words",
+      "typo",
+      "proximity",
+      "attribute",
+      "sort",
+      "exactness"
+  ],
+  distinctAttribute: 'id',
+  searchableAttributes: [
+      'id',
+      'nombre',
+      'descripcion',
+      'timestamp',
+      'fechaProximoDeadline',
+      'tipo',
+      'granArea1'
+  ],
+  displayedAttributes: [
+      'id',
+      'timestamp',
+      'estatus',
+      'daysLeft',
+      'nombre',
+      'tipo',
+      'granArea1',
+      'granArea2',
+      'link',
+      'descripcion',
+      'fechaProximoDeadline'
+  ],
+  stopWords: [
+      'de'
+  ],
+  sortableAttributes: [
+      'nombre',
+      'id'
+  ],
+  synonyms: {
+      
+  },
+  typoTolerance: {
+      "enabled": true,
+      "minWordSizeForTypos": {
+        "oneTypo": 5,
+        "twoTypos": 9
+      },
+      "disableOnWords": [],
+      "disableOnAttributes": []
+  },
+  pagination: {
+      maxTotalHits: 100
+  },
+  faceting: {
+      "maxValuesPerFacet": 100,
+      "sortFacetValuesBy": {
+        "*": "alpha"
+      }
+  },
+  searchCutoffMs: 300
+})
+*/
+
+/* filtros */
+/* 
+client.index('proyectos')
+  .updateFilterableAttributes([
+    'estatus'
+  ]) 
+*/
+client.index('proyectos').search('', {
+  filter: 'estatus = "Vencido"'
+})
+
+/* offset */
